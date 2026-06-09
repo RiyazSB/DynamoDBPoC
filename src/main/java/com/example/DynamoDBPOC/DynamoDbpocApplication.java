2ae707c3b716c9259043072b89bd113b6caece11
Riyaz S B <riyaz.s.b@hp.com>
2026-06-09 10:57:47 +0530

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/example/DynamoDBPOC/DynamoDbpocApplication.java
@@ -0,0 +1,13 @@
+package com.example.DynamoDBPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DynamoDBPOC/DynamoDbpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DynamoDBPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
